49b81e97394331fd80087b6192e1ca1cd066458a
PharaohKJ <kato@phalanxware.com>
2013-05-01 19:22:29 +0900

    削除対応

--- imap-deleter.rb
+++ imap-deleter.rb
@@ -44,26 +44,23 @@ puts "対象: #{result.count}"
 while (message_ids = result.slice!(0,50)).count > 0
 	heads = imap.fetch(message_ids, ["ENVELOPE"])
 	heads.each do |head|
-		#pp head
 		subject = head.attr['ENVELOPE'].subject
 		if subject == nil then
 			puts '[NO SUBJECT]'
 		else
-			#guess = Kconv.guess(subject)
-			#puts CODE_NAME[guess]
 			begin
 				subject = stripSpam subject
-				#puts subject
 				puts subject.toutf8
 			rescue
 				puts "CATCH ERROR!!!!"
-				#puts subject.gsub(/^\[SPAM\] /,'').toutf8
 			end
 		end
 	end
+	imap.store(message_ids, "+FLAGS", [:Deleted])
 	message_ids.clear
 end
 
-#imap.expunge
+
+imap.expunge
 imap.disconnect
 
